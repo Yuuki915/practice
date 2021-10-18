7c617e75fdd9f8b1f9d8b5282db23d1f382f0103
Yuki Hori <yuyu@Yukis-MacBook-Pro.local>
2021-10-18 00:12:18 -0700

    add OptionList component

--- react_practice_004_bloodtype/src/App.js
+++ react_practice_004_bloodtype/src/App.js
@@ -2,17 +2,18 @@ import "./App.css";
 import Question from "./components/Question";
 import Option from "./components/Option";
 import Button from "./components/Button";
+import OptionList from "./OptionList";
 
 const questions = [
   { id: 1, ask1: "あんた何型なん？" },
   { id: 2, ask2: "相手は何型か知っとーと？" },
 ];
 const options = [
-  { id: 1, chose: "A型" },
-  { id: 2, chose: "B型" },
-  { id: 3, chose: "O型" },
-  { id: 4, chose: "AB型" },
-  { id: 5, chose: "知らん…" },
+  { id: 1, choose: "A型" },
+  { id: 2, choose: "B型" },
+  { id: 3, choose: "O型" },
+  { id: 4, choose: "AB型" },
+  { id: 5, choose: "知らん…" },
 ];
 
 function App() {
@@ -20,13 +21,16 @@ function App() {
     <div className="App">
       <div>血液型相性診断</div>
       {/* <Button /> */}
-      <Question questions={questions[0]}/>
-  {/* OPTIONS */}
+      <Question questions={questions[0]} />
+      {/* OPTIONS */}
       <Option options={options[0]} />
       <Option options={options[1]} />
       <Option options={options[2]} />
       <Option options={options[3]} />
       <Option options={options[4]} />
+
+      {/* <OptionList OptionList={options}/> */}
+
       <Button />
     </div>
   );
---
+++ react_practice_004_bloodtype/src/OptionList.js
@@ -0,0 +1,10 @@
+// import Option from "./components/Option";
+
+// const OptionList = (props) => {
+//     const options = props.optionList.map((choose) => {
+//         return <Option key={choose.id} choose={choose}/>
+//     })
+//   return options;
+// };
+
+// export default OptionList;
