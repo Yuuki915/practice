047bc78538bae52d9f24f19d3fc62b425a6d0716
Yuki Hori <yuyu@Yukis-MacBook-Pro.local>
2021-10-17 22:26:24 -0700

    add react bloodtype file

---
+++ react_practice_004_bloodtype/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const 
+
+function App() {
+  return (
+    <div className="App">
+      <div>血液型相性診断</div>
+
+      <button />
+
+      <question />
+      <option />
+      <button />
+    </div>
+  );
+}
+
+export default App;
