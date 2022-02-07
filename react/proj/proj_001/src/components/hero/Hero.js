01dd5f8cf2db2b7589bf9535fa5e0690e64d6e5a
Yuuki915 <aso091335@gmail.com>
2022-02-06 23:38:13 -0800

    add

--- react/proj/proj_001/src/components/hero/Hero.js
+++ react/proj/proj_001/src/components/hero/Hero.js
@@ -1,16 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import heroImg from "../imgs/hero.jpg";
+import styled from "styled-components";
 import "./Hero.css";
 
-function Hero() {
+const Hero = () => {
+  const [imgScale, setImgScale] = useState(1);
+  const isBigger = () => {
+    setImgScale(imgScale + 0.1);
+  };
+
+  const HeroImg = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-image: url("${heroImg}");
+    background-size: cover;
+    background-position: 40% 50%;
+    transform: scale(${imgScale});
+  `;
+
   return (
     <div className="hero">
-      <img className="heroImg" src={heroImg} alt="" />
+      <HeroImg onScroll={isBigger} />
       <h1 className="siteTitle">
         <span>Yuuki Hori</span>
       </h1>
     </div>
   );
-}
+};
 
 export default Hero;
