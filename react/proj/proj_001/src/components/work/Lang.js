c61b612b29e5a32898bb42a345a14f9aaac5ec55
Yuuki915 <aso091335@gmail.com>
2022-03-10 23:57:40 -0800

    add Lang file

---
+++ react/proj/proj_001/src/components/work/Lang.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Lang(lang) {
+  return (
+    <div className="langs">
+      <div className="lang">{props.lang}</div>
+    </div>
+  );
+}
--- react/proj/proj_001/src/components/work/Work.js
+++ react/proj/proj_001/src/components/work/Work.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Lang from "./Lang";
 
 import "./Work.css";
 
@@ -11,7 +12,10 @@ function Work(props) {
           <h2>{props.title}</h2>
         </div>
         <div className="titles">
-          <p>{props.text}</p>
+          <p>
+            <Lang />
+            {props.text}
+          </p>
         </div>
       </div>
     </div>
