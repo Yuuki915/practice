a5cfd825e445df70e384d90387af180ef4440769
Yuuki915 <aso091335@gmail.com>
2022-03-16 01:44:21 -0700

    add file bookingym

---
+++ react/proj/bookingym/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { client } from "./client";
+
+class App extends React.Component {
+  state = {
+    articles: [],
+  };
+
+  componentDidMount() {
+    client
+      .getEntries()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(console.error);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <header>
+            <div className="wrapper">
+              <span>React and Contentful</span>
+            </div>
+          </header>
+          <main>
+            <div className="wrapper"></div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
